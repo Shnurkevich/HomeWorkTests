83cdc50c2700720b8f3aa0edf38461d8174da5ef
Aliaksandr Shnurkevich <65668318+Shnurkevich@users.noreply.github.com>
2021-04-08 17:32:28 +0300

    Add files via upload

---
+++ CircleCalculations/by.htp.task01.main/Circle.java
@@ -0,0 +1,39 @@
+package by.htp.task01.entity;
+
+public class Circle {
+    private double square;
+    private double perimeter;
+    private double radius;
+
+
+    public Circle() {
+    }
+
+    public Circle(double radius){
+        this.radius = radius;
+    }
+
+    public double getSquare() {
+        return square;
+    }
+
+    public void setSquare(double square) {
+        this.square = square;
+    }
+
+    public double getPerimeter() {
+        return perimeter;
+    }
+
+    public void setPerimeter(double perimeter) {
+        this.perimeter = perimeter;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+}
