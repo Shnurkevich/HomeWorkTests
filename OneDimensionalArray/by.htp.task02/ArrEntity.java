2423d048f78c662e173d604ea60127a6c927e553
Shnurkevich <65668318+Shnurkevich@users.noreply.github.com>
2021-04-08 13:26:45 +0300

    Add files via upload

---
+++ OneDimensionalArray/by.htp.task02/ArrEntity.java
@@ -0,0 +1,23 @@
+package by.htp.task02.entity;
+
+public class ArrEntity {
+
+    private int arr[];
+
+
+    public ArrEntity() {
+    }
+
+
+    public int[] getArr() {
+        return arr;
+    }
+
+    public void setArr(int[] arr) {
+        this.arr = arr;
+    }
+
+
+}
+
+
