27646f48e4547def61ec9327bb89fa3565602d49
Aliaksandr Shnurkevich <65668318+Shnurkevich@users.noreply.github.com>
2021-04-11 15:37:24 +0300

    Delete OneDimensionalArray directory

--- OneDimensionalArray/by.htp.task02.test/SmokeSuite.java
+++
@@ -1,9 +0,0 @@
-import org.junit.runner.RunWith;
-import org.junit.runners.Suite;
-
-@Suite.SuiteClasses({LogicTest.class, PresentationTest.class})
-@RunWith(Suite.class)
-
-public class SmokeSuite {
-
-}
