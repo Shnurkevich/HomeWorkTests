00ab6e58beb1a779b13d1c3309e0106d0ae75989
Aliaksandr Shnurkevich <65668318+Shnurkevich@users.noreply.github.com>
2021-04-11 15:37:42 +0300

    Delete CircleCalculations directory

--- CircleCalculations/by.htp.task01.test/SmokeSuite.java
+++
@@ -1,9 +0,0 @@
-import org.junit.runner.RunWith;
-import org.junit.runners.Suite;
-
-@Suite.SuiteClasses({CalcCircuitLogicTest.class, CalcSquareLogicTest.class, PresentationTest.class})
-@RunWith(Suite.class)
-
-public class SmokeSuite {
-
-}
