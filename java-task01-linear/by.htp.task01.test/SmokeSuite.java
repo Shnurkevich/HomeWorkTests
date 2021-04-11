f2c0534e1d3c7aae421a7913154f79aba26961d1
Aliaksandr Shnurkevich <65668318+Shnurkevich@users.noreply.github.com>
2021-04-11 15:46:59 +0300

    Create SmokeSuite.java

---
+++ java-task01-linear/by.htp.task01.test/SmokeSuite.java
@@ -0,0 +1,9 @@
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@Suite.SuiteClasses({CalcCircuitLogicTest.class, CalcSquareLogicTest.class, PresentationTest.class})
+@RunWith(Suite.class)
+
+public class SmokeSuite {
+
+}
