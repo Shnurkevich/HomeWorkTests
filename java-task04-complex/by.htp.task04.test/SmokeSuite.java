cca4612b9b0690d0d71028b796ab998b16447e78
Aliaksandr Shnurkevich <65668318+Shnurkevich@users.noreply.github.com>
2021-04-11 15:19:45 +0300

    Create SmokeSuite.java

---
+++ java-task04-complex/by.htp.task04.test/SmokeSuite.java
@@ -0,0 +1,11 @@
+package by.htp.task04.test;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@Suite.SuiteClasses({CutTest.class, SumTest.class})
+@RunWith(Suite.class)
+
+public class SmokeSuite {
+
+}
